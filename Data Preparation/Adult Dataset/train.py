e8e8a9ed5223a6c7cff29b228dc226490580b890
Ravi Shankar Prasad <144870420+ontherays@users.noreply.github.com>
2025-12-06 21:15:11 +0800

    Update and rename encode_categorical_variables_and_scale_numeric.py to train.py

---
+++ Data Preparation/Adult Dataset/train.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+X_train.shape, X_test.shape
