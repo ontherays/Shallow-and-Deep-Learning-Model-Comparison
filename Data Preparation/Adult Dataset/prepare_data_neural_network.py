7bfcef305b6ee933e1bc415d37179993847dcea6
Ravi Shankar Prasad <144870420+ontherays@users.noreply.github.com>
2025-12-06 21:16:05 +0800

    Update and rename Transform_features_into_NumPy.py to prepare_data_neural_network.py

---
+++ Data Preparation/Adult Dataset/prepare_data_neural_network.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+X_train = X_train.to_numpy().astype("float32")
+X_test = X_test.to_numpy().astype("float32")
+y_train = y_train.to_numpy().astype("float32")
+y_test = y_test.to_numpy().astype("float32")
