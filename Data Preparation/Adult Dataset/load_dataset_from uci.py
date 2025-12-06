36847d2904f4130207f919ff2c02ec340317ef02
Ravi Shankar Prasad <144870420+ontherays@users.noreply.github.com>
2025-12-06 21:12:47 +0800

    Update load_dataset_from uci.py

--- Data Preparation/Adult Dataset/load_dataset_from uci.py
+++ Data Preparation/Adult Dataset/load_dataset_from uci.py
@@ -1,17 +1,11 @@
 import pandas as pd
 
-# Load Adult dataset directly from UCI
-column_names = [
-    "age","workclass","fnlwgt","education","education-num","marital-status",
-    "occupation","relationship","race","sex","capital-gain","capital-loss",
-    "hours-per-week","native-country","income"
+adult_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+col_names = [
+    'age','workclass','fnlwgt','education','education-num','marital-status','occupation',
+    'relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country','income'
 ]
 
-url_train = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
-url_test = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test"
+df_adult = pd.read_csv(adult_url, names=col_names, skipinitialspace=True)
 
-df_train = pd.read_csv(url_train, names=column_names, na_values=" ?", skipinitialspace=True)
-df_test = pd.read_csv(url_test, names=column_names, na_values=" ?", skipinitialspace=True, skiprows=1)
-
-df = pd.concat([df_train, df_test], ignore_index=True)
-df.head()
+df_adult.head()
